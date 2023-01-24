7daf70735478c8a37d3a93a6456dd94ca85d2206
Daniel Lopez <daniellopezj0327@gmail.com>
2023-01-24 18:41:47 -0500

    start navbar

--- src/components/common/nav/nav.tsx
+++ src/components/common/nav/nav.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import navStyles from '@/components/common/nav/Nav.module.css'
+import Image from 'next/image'
 
-const nav = () => {
+const Nav = () => {
   return (
-    <div>nav</div>
+    <div className={navStyles.main}>
+         <Image
+            className={navStyles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+      <span> icon </span>
+    </div>
   )
 }
 
-export default nav
+export default Nav
