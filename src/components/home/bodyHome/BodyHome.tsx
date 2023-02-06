a1ac31910e4a8d5d2cdebbe7415cf7ac00ba770a
Daniel Lopez <daniellopezj0327@gmail.com>
2023-02-06 18:46:35 -0500

    add cards to home

--- src/components/home/bodyHome/BodyHome.tsx
+++ src/components/home/bodyHome/BodyHome.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import CardHome from '@/components/home/cardHome/CardHome'
+import { cardsHome } from '@/services/data.mocks'
 
 const BodyHome = () => {
   return (
-    <div>
-    <CardHome/>
+    <div >
+      {cardsHome.map((item)=>(
+        <CardHome key={item.id}  {...item}/>
+      ))}
     </div>
   )
 }
