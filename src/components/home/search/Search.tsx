9841e7b9846e4e1929b742a4c729d77e4a9e6220
Daniel Lopez <daniellopezj0327@gmail.com>
2023-03-17 08:11:27 -0500

    create buttons vender y alquilar properties

--- src/components/home/search/Search.tsx
+++ src/components/home/search/Search.tsx
@@ -2,8 +2,26 @@ import React, {useState} from 'react'
 import localStyles from '@/components/home/search/Search.module.css'
 const Search = () => {
 
-  const [query, setQuery] = useState("");
+  const optionsRent =[
+    {
+      id:0,
+      value:'sale',
+      text:'vender'
+    },
+    {
+      id:0,
+      value:'rent',
+      text:'Alquilar'
+    }
+  ]
 
+
+  const [query, setQuery] = useState("");
+  const [selectTypeService, setSelectTypeService] = useState("");
+
+  const handleSelectType = (event: any) => {
+    setSelectTypeService(event.target.value);
+  };
   const handleQueryChange = (event: any) => {
     console.log(event)
     setQuery(event.target.value);
@@ -15,14 +33,29 @@ const Search = () => {
 
   return (
     <div className={localStyles.mainContainerSearch}>
-     <div className={localStyles.title}>
-      <h2>¿Realmente compras algo para toda la vida?</h2>
-      <div>
+      <div className={localStyles.title}>
+        <h2>¿Realmente compras algo para toda la vida?</h2>
+      </div>
+      <div className={localStyles.searchContainerLine}>
+        <div>
+         {optionsRent.map((option)=>(
+          <label key={option.id} htmlFor={option.value}>
+            <input
+              className={localStyles.customInput}
+              id={option.value}
+              type="radio"
+              value={option.value}
+              checked={selectTypeService ===  option.value}  
+              onChange={handleSelectType}
+            />
+            {option.text}
+          </label>
+         ))}
+        </div>
         <input type="text" value={query} onChange={handleQueryChange} />
         <button onClick={handleButtonClick}>Go</button>
       </div>
       <h1>{query}</h1>
-     </div>
     </div>
   )
 }
