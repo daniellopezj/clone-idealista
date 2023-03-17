b541dd5c532dfe9509d14dd47175b4218c46a92f
Daniel Lopez <daniellopezj0327@gmail.com>
2023-03-16 20:12:15 -0500

    create seeker home

--- src/components/home/search/Search.tsx
+++ src/components/home/search/Search.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import localStyles from '@/components/home/search/Search.module.css'
 const Search = () => {
+
+  const [query, setQuery] = useState("");
+
+  const handleQueryChange = (event: any) => {
+    console.log(event)
+    setQuery(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log(query)
+  };
+
   return (
     <div className={localStyles.mainContainerSearch}>
      <div className={localStyles.title}>
-
       <h2>¿Realmente compras algo para toda la vida?</h2>
+      <div>
+        <input type="text" value={query} onChange={handleQueryChange} />
+        <button onClick={handleButtonClick}>Go</button>
+      </div>
+      <h1>{query}</h1>
      </div>
     </div>
   )
