9f1570af72e1fbadcfc359bbfc23f1abef7705f9
Daniel Lopez <daniellopezj0327@gmail.com>
2023-05-02 18:57:12 -0500

    add list of places in search component

--- src/components/home/search/Search.tsx
+++ src/components/home/search/Search.tsx
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import localStyles from '@/components/home/search/Search.module.css';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+interface SearchResult {
+  name: string;
+  locationId: string;
+  divisible: boolean;
+  type: string;
+  suggestedLocationId: string;
+  subTypeText: string;
+}
 
 const Search = () => {
   const optionsRent = [
@@ -19,13 +28,41 @@ const Search = () => {
 
   const [query, setQuery] = useState('');
   const [selectTypeService, setSelectTypeService] = useState('rent');
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+  const [showList, setShowList] = useState<boolean>(false);
 
   const handleSelectType = (event: any) => {
     setSelectTypeService(event.target.value);
   };
-  const handleQueryChange = (event: any) => {
+  const handleQueryChange = async (event: any) => {
     setQuery(event.target.value);
   };
+
+  useEffect(() => {
+    const getData = async () => {
+      console.log(query);
+      try {
+        const res = await fetch(
+          `https://idealista2.p.rapidapi.com/auto-complete?prefix=${query}&country=es`,
+          {
+            headers: {
+              'X-RapidAPI-Key':
+                '4e66ee2754msh37d0a809cfca29dp18dfa2jsnd7927de5ae36',
+              'X-RapidAPI-Host': 'idealista2.p.rapidapi.com',
+            },
+          },
+        );
+
+        const results = await res.json();
+
+        setSearchResults(results.locations);
+        console.log(results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [query]);
 
   const handleButtonClick = () => {
     console.log(query);
@@ -61,15 +98,30 @@ const Search = () => {
               </label>
             ))}
           </div>
-          <div>
+          <div className={localStyles.containerInputSearch}>
             <input
               className={localStyles.customInputSearch}
               type="text"
               placeholder="Escribe dónde buscas"
               value={query}
               onChange={handleQueryChange}
+              onFocus={() => setShowList(true)}
+              onBlur={() => setTimeout(() => setShowList(false), 200)}
             />
             <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+            {showList ? (
+              <div className={localStyles.containerList}>
+                {searchResults.length > 0 && (
+                  <ul className="search-component-results">
+                    {searchResults.map((result) => (
+                      <li key={result.locationId}>{JSON.stringify(result)} </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ) : (
+              ''
+            )}
           </div>
           <button
             className={localStyles.customButton}
