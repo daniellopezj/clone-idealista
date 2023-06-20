cbffdbe3f808d96e1fa88e1316b3008466e7c996
Daniel Lopez <daniellopezj0327@gmail.com>
2023-06-20 12:49:36 -0500

    add style in responsive design

--- src/components/common/base/carousel/BaseCarousel.tsx
+++ src/components/common/base/carousel/BaseCarousel.tsx
@@ -3,21 +3,24 @@ import Image from 'next/image';
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import localStyle from '@/components/common/base/carousel/BaseCarousel.module.scss';
 
 interface BaseCarouselProps {
   images: ImagesCarousel[];
   className: string;
+  itemImageClassName: string;
   carouselWidth?: string;
 }
 
 const BaseCarousel = ({
   images,
   className,
+  itemImageClassName,
   carouselWidth = '300px',
 }: BaseCarouselProps) => {
   return (
     <Carousel
-      className={className}
+      className={`${localStyle.itemCarousel} ${className} `}
       width={carouselWidth}
       dynamicHeight={true}
       animationHandler={'slide'}
@@ -25,7 +28,7 @@ const BaseCarousel = ({
       showIndicators={false}
     >
       {images.map((image, i) => (
-        <Image key={`image${i}`} alt="Floor image" src={image.url} width={600} height={600} />
+        <Image key={`image${i}`} className={itemImageClassName} alt="Floor image" src={image.url} width={600} height={200} />
       ))}
     </Carousel>
   );
