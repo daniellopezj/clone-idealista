d2d67a99a5bce408daf11f7311447716056eafed
Daniel Lopez <daniellopezj0327@gmail.com>
2023-06-23 09:12:56 -0500

    process

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -4,25 +4,39 @@ import {
   ResponsePlaces,
   SearchResult,
 } from '@/types/Places.types';
-import axios from 'axios';
+import axios, { CancelTokenSource } from 'axios';
 
+let cancelTokenSource: CancelTokenSource | null = null;
 export const backend = () => {
+  const BASE_URL = process.env.PUBLIC_API_BASE_URL;
   const headers = {
-    'X-RapidAPI-Key': 'b92e6ba8e6mshc10c2bd9fb133a8p1ad5e8jsnded8e7917a1a',
-    'X-RapidAPI-Host': 'idealista2.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.PUBLIC_API_KEY,
+    'X-RapidAPI-Host': process.env.PUBLIC_API_HOST,
   };
 
-  const BASE_URL = 'https://idealista2.p.rapidapi.com';
   async function apiSearch(query: string): Promise<SearchResult[]> {
-    const res = await axios.get(
-      `${BASE_URL}/auto-complete?prefix=${query}&country=es`,
-      {
-        headers: headers,
-      },
-    );
-    if (res.status < 400) {
-      const results: ResponsePlaces = res.data;
-      return results.locations;
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('Previous request cancelled due to new request');
+    }
+    cancelTokenSource = axios.CancelToken.source();
+    try {
+
+
+      const res = await axios.get(
+        `${BASE_URL}/auto-complete?prefix=${query}&country=es`,
+        {
+          headers: headers,
+          cancelToken: cancelTokenSource.token
+        },
+      );
+      if (res.status < 400) {
+        const results: ResponsePlaces = res.data;
+        return results.locations;
+      }
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        console.log('Request cancelled:', error.message);
+      }
     }
     return [];
   }
