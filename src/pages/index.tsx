b9f5aa8fd221fd23b86721329a232d42b5ce1bbb
Daniel Lopez <daniellopezj0327@gmail.com>
2023-07-06 11:09:49 -0500

    add twitter SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,30 +13,50 @@ export default function Home() {
           name="description"
           content="Clone idealista desarrollado con Next js y react js. Sistema para la comercialización de viviendas en toda España. "
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta
+          name="keywords"
+          content="fullStack, desarrollador freelance,HTML/CSS, JavaScript, TypeScript, angular, vue, react,Node js,, freelance, frontend, front-end, Desarrollador frontend, programador, Desarrollo web, Responsive web design, UI/UX"
+        />
+
+        <meta name="type" content="website" />
+        <meta name="site_name" content="clone-idealista.vercel.app" />
+        <meta
+          property="og:title"
+          content="Clone Idealista — Casas y pisos, alquiler y venta"
+        />
         <meta
           property="og:description"
           content="Clone idealista desarrollado con Next js y react js. Sistema para la comercialización de viviendas en toda España. "
         />
         <meta
-          property="og:title"
-          content="Clone Idealista — Casas y pisos, alquiler y venta"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta
           property="og:image"
           content="https://clone-idealista.vercel.app/mainView.webp"
         />
-        <meta
-          name="keywords"
-          content="fullStack, desarrollador freelance,HTML/CSS, JavaScript, TypeScript, angular, vue, react,Node js,, freelance, frontend, front-end, Desarrollador frontend, programador, Desarrollo web, Responsive web design, UI/UX"
-        />
-        <meta property="og:image:width" content="1000" />
+        <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="600" />
         <meta property="og:type" content="website" />
-        <meta name="type" content="website" />
-        <meta name="site_name" content="clone-idealista.vercel.app" />
+
         <meta property="og:url" content="https://clone-idealista.vercel.app" />
+        <meta
+          property="twitter:title"
+          content="Clone Idealista — Casas y pisos, alquiler y venta"
+        />
+        <meta
+          property="twitter:description"
+          content="Clone idealista desarrollado con Next js y react js. Sistema para la comercialización de viviendas en toda España. "
+        />
+        <meta
+          property="twitter:image"
+          content="https://clone-idealista.vercel.app/mainView.webp"
+        />
+        <meta property="twitter:image:width" content="1200" />
+        <meta property="twitter:image:height" content="600" />
+        <meta property="twitter:type" content="website" />
+
+        <meta property="twitter:url" content="https://clone-idealista.vercel.app" />
         <link rel="canonical" href="https://clone-idealista.vercel.app" />
         <link
           rel="icon"
