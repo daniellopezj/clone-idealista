b42a6e2398095058a6c23d3a1302ed005bfb369a
Daniel Lopez <daniellopezj0327@gmail.com>
2024-11-19 09:53:06 +0100

    fix bug in images

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Fib(0) -> 0
+Fib(1) -> 1
+Fib(n) -> Fib(n-1) + Fib(n-2)
+ * 
+primera condicion recibe un 
+
+Fib(35) -> Fib(34) + Fib (33)
+0 1 2 3 4 5 6 7  8  9  10 11
+0 1 1 2 3 5 8 13 21 34 55 89
+
+fib(4) -> fib(3) + fib(2)
+*/
--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['img3.idealista'],
+    domains: ['img3.idealista','img4.idealista', ],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'img3.idealista.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img4.idealista.com',
         port: '',
         pathname: '/**',
       },
